d78e6192dc762ec03d893207dd2bba0dd37a0d82
Dipan Nanda <43346471+themagicalmammal@users.noreply.github.com>
2023-01-03 15:47:59 +0000

    1.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
-VERSION = "1.0.7"
+VERSION = "1.0.8"
 DESCRIPTION = "Optimised way to write in Excel files."
 
 with open("requirements.txt") as f:
@@ -23,8 +23,19 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
+    # url="https://github.com/themagicalmammal/excel-write",
     packages=find_packages(),
-    install_requires=required,
+    include_package_data=True,
+    license="MIT",
+    install_requires=required,    
+    project_urls={
+        # "Website": "https://github.com/themagicalmammal/excel-write",
+        "Source code": "https://github.com/themagicalmammal/excel-write",
+        "Documentation":
+        "https://github.com/themagicalmammal/excel-write/blob/main/README.md",
+        "Bug tracker":
+        "https://github.com/themagicalmammal/excel-write/issues",
+    },
     keywords=[
         "python",
         "excel",
@@ -37,9 +48,13 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
+        "Topic :: Utilities",
     ],
 )
